4e5b936072c794b367bb7700805952ab6c88be79
Alex Nichol <unixpickle@gmail.com>
2017-09-27 10:11:58 -0700

    import CNN in root models dir

--- anyrl/models/__init__.py
+++ anyrl/models/__init__.py
@@ -3,7 +3,7 @@ Various models for Reinforcement Learning agents.
 """
 
 from .base import Model, TFActorCritic
-from .feedforward import FeedforwardAC, MLP
+from .feedforward import FeedforwardAC, MLP, CNN
 from .recurrent import RecurrentAC, RNNCellAC
 
 __all__ = dir()

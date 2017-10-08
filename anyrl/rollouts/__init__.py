a1b1fbda4cd5b393855ec5233d3292b56977d699
Alex Nichol <unixpickle@gmail.com>
2017-10-07 20:33:22 -0700

    utility to compute mean reward of finished episodes

--- anyrl/rollouts/__init__.py
+++ anyrl/rollouts/__init__.py
@@ -4,7 +4,7 @@ Various ways to gather trajectories in RL environments.
 
 from .env import (batched_gym_env, AsyncEnv, BatchedEnv, AsyncGymEnv,
                   BatchedAsyncEnv, BatchedGymEnv)
-from .list import mean_total_reward
+from .list import mean_total_reward, mean_finished_reward
 from .norm import RewardNormalizer
 from .rollers import Roller, BasicRoller, TruncatedRoller, EpisodeRoller
 from .rollout import Rollout, empty_rollout
--- anyrl/rollouts/list.py
+++ anyrl/rollouts/list.py
@@ -7,3 +7,13 @@ def mean_total_reward(rollouts):
     Get the mean of the total rewards.
     """
     return sum([r.total_reward for r in rollouts]) / len(rollouts)
+
+def mean_finished_reward(rollouts):
+    """
+    Get the mean of the total rewards in all completed
+    episodes.
+    """
+    complete = [r for r in rollouts if not r.trunc_end]
+    if not complete:
+        return 0.0
+    return sum([r.total_reward for r in complete]) / len(complete)

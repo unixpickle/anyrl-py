586c3bb864792ec0ec8c2fa25ddbc6a32f252180
Alex Nichol <unixpickle@gmail.com>
2017-12-14 13:46:08 -0800

    less strict cloudpickle version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.10.3',
+    version='0.10.4',
 
     description='A library for Reinforcement Learning',
     long_description=LONG_DESCRIPTION,
@@ -75,7 +75,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'numpy>=1.0.0,<2.0.0',
-        'cloudpickle>=0.4.0,<0.5.0',
+        'cloudpickle>=0.4.0',
         'gym>=0.9.2,<0.10.0',
         'pandas>=0.20.0,<0.21.0'
     ],

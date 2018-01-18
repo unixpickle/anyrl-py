26f5268f3a2aaa05ec7635f186451ba34e06d730
Alex Nichol <unixpickle@gmail.com>
2018-01-18 11:00:20 -0800

    add TFQNetwork base class

--- anyrl/models/base.py
+++ anyrl/models/base.py
@@ -99,3 +99,67 @@ class TFActorCritic(Model):
           batch_size: the approximate mini-batch size
         """
         pass
+
+class TFQNetwork(Model):
+    """
+    A Q-network model which is differentiable via
+    TensorFlow.
+
+    Attributes:
+      session: the TF session for the model.
+      num_actions: the size of the discrete action space.
+      obs_vectorizer: used to convert observations into
+        Tensors.
+      name: the variable scope name for the network.
+      variables: the trainable variables of the network.
+
+    When a Q-network is instantiated, a version of the
+    model is created for the step() method.
+    This involves creating a set of variables and
+    placeholders in the graph.
+    After construction, other Q-network methods like
+    transition_loss() may reuse variables.
+    """
+    def __init__(self, session, num_actions, obs_vectorizer, name):
+        """
+        Construct a Q-network.
+
+        Args:
+          session: the TF session used by step().
+          num_actions: the number of possible actions.
+          obs_vectorizer: a vectorizer for the observation
+            space.
+          name: the scope name for the model. This should
+            be different for the target and online models.
+        """
+        self.session = session
+        self.num_actions = num_actions
+        self.obs_vectorizer = obs_vectorizer
+        self.name = name
+        self.variables = []
+
+    # pylint: disable=R0913
+    @abstractmethod
+    def transition_loss(self, target_net, obses, actions, rews, new_obses, discounts):
+        """
+        Create a loss term for the Bellman update.
+
+        This should be called on the "online network",
+        since the variables of `self` are trained.
+
+        Args:
+          target_net: the companion TFQNetwork from which
+            the target Q values should be computed.
+          obses: the Tensor of starting observations.
+          actions: the 1-D int32 Tensor of actions.
+          rews: the 1-D Tensor of rewards.
+          new_obses: the Tensor of final observations.
+          discounts: the 1-D Tensor of discount factors.
+            For n-step Q-learning, this contains the true
+            discount factor raised to the n-th power.
+
+        Returns:
+          A 1-D Tensor containing a loss value for each
+            transition in the batch of transitions.
+        """
+        pass

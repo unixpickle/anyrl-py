f95e511873b9c8fbfa0c6b0419f77df958f5e438
Alex Nichol <unixpickle@gmail.com>
2018-01-19 12:55:58 -0800

    add RandomAgent model

--- anyrl/models/__init__.py
+++ anyrl/models/__init__.py
@@ -4,6 +4,7 @@ Various models for Reinforcement Learning agents.
 
 from .base import Model, TFActorCritic
 from .feedforward_ac import FeedforwardAC, MLP, CNN
+from .misc import RandomAgent
 from .recurrent_ac import RecurrentAC, RNNCellAC
 
 __all__ = dir()

bd7584ac8f7598f0993dd715b937c4b854a7e3d7
Alex Nichol <unixpickle@gmail.com>
2018-01-19 11:41:12 -0800

    remove deleted import

--- anyrl/models/__init__.py
+++ anyrl/models/__init__.py
@@ -4,8 +4,7 @@ Various models for Reinforcement Learning agents.
 
 from .base import Model, TFActorCritic
 from .feedforward_ac import FeedforwardAC, MLP, CNN
-from .q_networks import (ScalarQNetwork, MLPQNetwork, NatureQNetwork, DuelingQNetwork,
-                         EpsGreedyQNetwork)
+from .q_networks import ScalarQNetwork, MLPQNetwork, NatureQNetwork, EpsGreedyQNetwork
 from .recurrent_ac import RecurrentAC, RNNCellAC
 
 __all__ = dir()

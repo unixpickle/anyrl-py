df8b3ef39e5fc406fe882c788d57e228b6ba7e89
Alex Nichol <unixpickle@gmail.com>
2018-01-18 17:52:40 -0800

    add missing import

--- anyrl/models/__init__.py
+++ anyrl/models/__init__.py
@@ -4,7 +4,8 @@ Various models for Reinforcement Learning agents.
 
 from .base import Model, TFActorCritic
 from .feedforward_ac import FeedforwardAC, MLP, CNN
-from .q_networks import ScalarQNetwork, NatureQNetwork, DuelingQNetwork, EpsGreedyQNetwork
+from .q_networks import (ScalarQNetwork, MLPQNetwork, NatureQNetwork, DuelingQNetwork,
+                         EpsGreedyQNetwork)
 from .recurrent_ac import RecurrentAC, RNNCellAC
 
 __all__ = dir()

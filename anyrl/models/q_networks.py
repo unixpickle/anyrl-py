d7ec32f757c09462181a4e004b53898aafe21db3
Alex Nichol <unixpickle@gmail.com>
2018-01-21 19:28:28 -0800

    reward distribution API

--- anyrl/models/q_networks.py
+++ anyrl/models/q_networks.py
@@ -10,7 +10,7 @@ import numpy as np
 import tensorflow as tf
 
 from .base import TFQNetwork
-from .util import nature_cnn, simple_mlp, take_vector_elems
+from .util import nature_cnn, simple_mlp, take_vector_elems, put_vector_elems
 
 # pylint: disable=R0913,R0903
 
@@ -248,3 +248,56 @@ def _factorized_noise(inputs, outputs):
 
 def _signed_sqrt(values):
     return tf.sqrt(tf.abs(values)) * tf.sign(values)
+
+class DiscreteDist:
+    """
+    A discrete reward distribution.
+    """
+    def __init__(self, num_atoms, min_val, max_val):
+        assert num_atoms >= 2
+        assert max_val > min_val
+        self.num_atoms = num_atoms
+        self.min_val = min_val
+        self.max_val = max_val
+        self._delta = (self.max_val - self.min_val) / (self.num_atoms - 1)
+
+    def atom_values(self):
+        """Get the reward values for each atom."""
+        return [self.min_val + i * self._delta for i in range(0, self.num_atoms)]
+
+    def mean(self, log_probs):
+        """Get the mean rewards for the distributions."""
+        return tf.reduce_sum(tf.exp(log_probs) * tf.constant(self.atom_values), axis=-1)
+
+    def add_rewards(self, log_probs, rewards):
+        """
+        Compute new distributions after adding rewards to
+        old distributions.
+
+        Args:
+          log_probs: a batch of log probability vectors.
+          rewards: a batch of rewards.
+
+        Returns:
+          A new batch of log probability vectors.
+        """
+        minus_inf = tf.zeros_like(log_probs) - tf.constant(np.inf)
+        new_probs = minus_inf
+        for i, atom_rew in enumerate(self.atom_values()):
+            old_probs = log_probs[:, i]
+            # If the position is exactly 0, rounding up
+            # and subtracting 1 would cause problems.
+            new_idxs = tf.clip_by_value(((rewards + atom_rew) - self.min_val) / self._delta,
+                                        1e-5, float(self.num_atoms - 1))
+            index1 = tf.cast(tf.ceil(new_idxs) - 1, tf.int32)
+            index2 = tf.cast(tf.ceil(new_idxs), tf.int32)
+            frac1 = tf.abs(tf.ceil(new_idxs) - new_idxs)
+            for indices, frac in [(index1, frac1), (index2, 1 - frac1)]:
+                prob_offset = put_vector_elems(indices, old_probs - 1 + tf.log(frac),
+                                               self.num_atoms)
+                prob_offset = tf.where(tf.equal(prob_offset, 0), minus_inf, prob_offset + 1)
+                new_probs = _add_log_probs(new_probs, prob_offset)
+        return new_probs
+
+def _add_log_probs(probs1, probs2):
+    return tf.reduce_logsumexp(tf.stack([probs1, probs2]), axis=0)

b072c6d7bcf4d846e35ec48ab4f2b595d384030e
Alex Nichol <unixpickle@gmail.com>
2018-01-22 09:59:24 -0800

    separate files for scalar/dist DQN

--- anyrl/models/__init__.py
+++ anyrl/models/__init__.py
@@ -5,7 +5,7 @@ Various models for Reinforcement Learning agents.
 from .base import Model, TFActorCritic
 from .feedforward_ac import FeedforwardAC, MLP, CNN
 from .misc import RandomAgent
-from .q_networks import (ScalarQNetwork, MLPQNetwork, NatureQNetwork, EpsGreedyQNetwork,
+from .dqn_scalar import (ScalarQNetwork, MLPQNetwork, NatureQNetwork, EpsGreedyQNetwork,
                          noisy_net_dense)
 from .recurrent_ac import RecurrentAC, RNNCellAC
 

6d14b9d4da48db9deaf9383324e4d3f05d34d41c
Alex Nichol <unixpickle@gmail.com>
2018-01-23 15:14:11 -0800

    add rainbow_models to anyrl.models module

--- anyrl/models/__init__.py
+++ anyrl/models/__init__.py
@@ -5,7 +5,7 @@ Various models for Reinforcement Learning agents.
 from .base import Model, TFActorCritic
 from .feedforward_ac import FeedforwardAC, MLP, CNN
 from .misc import RandomAgent
-from .dqn_dist import DistQNetwork, MLPDistQNetwork, NatureDistQNetwork
+from .dqn_dist import DistQNetwork, MLPDistQNetwork, NatureDistQNetwork, rainbow_models
 from .dqn_scalar import (ScalarQNetwork, MLPQNetwork, NatureQNetwork, EpsGreedyQNetwork,
                          noisy_net_dense)
 from .recurrent_ac import RecurrentAC, RNNCellAC

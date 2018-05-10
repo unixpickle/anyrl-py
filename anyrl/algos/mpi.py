fa141f728e5ae9931d5a5a8201f6071c456493fa
Alex Nichol <unixpickle@gmail.com>
2018-05-10 11:24:05 -0700

    use placeholders for MPI sync
    fixes #15

--- anyrl/algos/mpi.py
+++ anyrl/algos/mpi.py
@@ -29,8 +29,9 @@ class MPIOptimizer:
             self.placeholders.append(placeholder)
             apply_in.append((placeholder, var))
         self.apply = optimizer.apply_gradients(apply_in)
-        self._optimizer_vars = [v for v in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-                                if v not in old_variables]
+        optimizer_vars = [v for v in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+                          if v not in old_variables]
+        self._var_sync = _VarSync([v for _, v in self.grads] + optimizer_vars)
 
     def minimize(self, sess, feed_dict=None, terms=None):
         """
@@ -76,21 +77,36 @@ class MPIOptimizer:
         """
         Send the root node's parameters to every worker.
 
-        This may add nodes to the graph, so it should not
-        be called an unbounded number of times.
-
         This synchronizes both the model parameters and
         any extra variables created by the optimizer.
 
         Arguments:
           sess: the TensorFlow session.
         """
+        self._var_sync.sync(sess)
+
+
+class _VarSync:
+    """
+    An object that can synchronize vars between MPI nodes.
+    """
+
+    def __init__(self, variables):
+        self._variables = variables
+        self._placeholders = [tf.placeholder(v.dtype.base_dtype, shape=v.get_shape())
+                              for v in variables]
+        self._assigns = [tf.assign(v, ph) for v, ph in zip(variables, self._placeholders)]
+
+    def sync(self, sess):
+        """
+        Synchronize all the variables.
+        """
         rank = MPI.COMM_WORLD.Get_rank()
-        for var in [v for _, v in self.grads] + self._optimizer_vars:
+        for var, ph, assign in zip(self._variables, self._placeholders, self._assigns):
             if rank == 0:
                 MPI.COMM_WORLD.bcast(sess.run(var))
             else:
-                sess.run(tf.assign(var, MPI.COMM_WORLD.bcast(None)))
+                sess.run(assign, feed_dict={ph: MPI.COMM_WORLD.bcast(None)})
 
 
 def mpi_ppo(ppo, optimizer, rollouts, batch_size=None, num_iter=12, log_fn=None,

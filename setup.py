c3bcb14c11411a3262d3f55ff50063f8f23cc89a
Alex Nichol <unixpickle@gmail.com>
2018-09-13 19:27:00 -0400

    loosen pandas requirement
    Fixes #34

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with codecs.open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='anyrl',
-    version='0.12.20',
+    version='0.12.21',
     description='A library for Reinforcement Learning',
     long_description=LONG_DESCRIPTION,
     url='https://github.com/unixpickle/anyrl-py',
@@ -30,7 +30,7 @@ setup(
         'numpy>=1.0.0,<2.0.0',
         'cloudpickle>=0.4.0',
         'gym>=0.9.6,<0.11.0',
-        'pandas>=0.20.0,<0.21.0'
+        'pandas>=0.20.0,<0.23.0'
     ],
     extras_require={
         "tf": ["tensorflow>=1.0.0"],
